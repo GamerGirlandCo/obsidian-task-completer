60a7097858d518b427784e3a8f633fa495dd6ea4
☙◦ The Tablet ❀ GamerGirlandCo ◦❧ <c.negrissa.the.angel@gmail.com>
2022-07-17 19:11:48 -0400

    works in latest obsidian version \o/

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
-import { Editor, MarkdownView, Plugin } from 'obsidian';
+import { Editor, MarkdownView, Plugin, editorViewField, editorEditorField } from 'obsidian';
+import { EditorView } from 'codemirror';
 import { getAPI, DataviewApi } from "obsidian-dataview";
 import {TaskUtil} from "./TaskUtil";
 // import {findTasksInFile, parseMarkdown, parsePage} from "obsidian-dataview/lib/data/file";
@@ -23,12 +24,12 @@ export default class MyPlugin extends Plugin {
 			editorCallback: async (editor: Editor, view: MarkdownView) => {
 				this.isWorking = true;
 				const activeFile = this.app.workspace.getActiveFile();
-				const curse = editor.offsetToPos(this.curnum).line;
+				const cursor = editor.offsetToPos(this.curnum).line;
 				const source = this.dvapi.page(activeFile.path).file
-				const children = source.tasks.values.filter((a: any) => a.parent === curse)
-				const parent = source.tasks.values.filter((a: any) => a.line === curse)[0]
-				editor.setCursor(curse)
-				this.subs(children).flat(Infinity).forEach(l => {
+				const children = source.tasks.values.filter((a: any) => a.parent === cursor)
+				const parent = source.tasks.values.filter((a: any) => a.line === cursor)[0]
+				editor.setCursor(cursor)
+				this.recurseSubtasks(children).flat(Infinity).forEach(l => {
 					this.dvapi.index.touch();
 					this.app.workspace.trigger("dataview:refresh-views");
 					let origLine = editor.getLine(parent.line)
@@ -41,29 +42,31 @@ export default class MyPlugin extends Plugin {
 						editor.setLine(l, edit);
 					}
 				})
+				console.log("hi.")
 				console.log(source.tasks.values)
-				console.log(curse)
+				console.log(cursor)
 				console.log(children, parent)
+				console.log(view)
+				console.log(editor)
 				/*const ws = this.app.workspace;
 				const cache = mcache.getFileCache(ws.getActiveFile())
 				// console.log(curs);
 				// console.log(cache)
 				const tasks = cache.listItems.filter(a => !!a.task)
 				const currchildren = tasks.filter(a => a.parent === curs.line)
-				curr
-				console.log(tasks)
-				console.log(currchildren)
-				console.log(view)
-				console.log(editor)*/
+				curr*/
 			}
 		});
-		this.registerDomEvent(document, 'click', async (evt: MouseEvent) => {
+		this.app.workspace.on("click", async (evt: MouseEvent) => {
 			// @ts-ignore
 			let tgt: EventTarget & HTMLElement & {cmView: any} = evt.target;
 			if(tgt.tagName.toLowerCase() !== "input" || !!tgt.getAttribute("disabled")) {
 				return
 			}
-			this.curnum = tgt.cmView.editorView.posAtDOM(tgt);
+			console.debug("tgt", tgt, editorViewField, editorEditorField)
+			let ev = EditorView.findFromDOM(document.body)
+
+			this.curnum = ev.posAtDOM(tgt);
 			this.dvapi.index.touch();
 			this.app.workspace.trigger("dataview:refresh-views");
 			tgt.setAttribute("disabled", "")
@@ -71,9 +74,14 @@ export default class MyPlugin extends Plugin {
 			tgt.removeAttribute("disabled")
 			
 			// @ts-ignore
-			this.app.commands.executeCommandById("obsidian-auto-checkbox:recursive-check-tik")
+			this.app.commands.executeCommandById("obsidian-auto-checkbox:recursive-tick")
 			
-		});
+		})
+		/* this.registerCodeMirror((cm) => {
+				
+		
+		}) */
+		/* this.registerDomEvent(document, 'click', async ); */
 	}
 	timeMe():Promise<void> {
 		return new Promise((res, rej) => {
@@ -82,18 +90,18 @@ export default class MyPlugin extends Plugin {
 		})
 	}
 	
-	subs(pa: any[]): Array<any> {
-		const la: any = [];
-		function tick(b: any) {
-			la.push(b.children.map(tick))
-			console.log("la", la, pa)
+	recurseSubtasks(tasks: any[]): Array<any> {
+		const arr: any = [];
+		function mapper(b: any) {
+			arr.push(b.children.map(mapper))
+			console.log("la", arr, tasks)
 			return b.line
 		}
-		pa.map(a => {
-			la.push(a.line)
+		tasks.map(a => {
+			arr.push(a.line)
 		})
-		pa.map(tick)
-		return la;
+		tasks.map(mapper)
+		return arr;
 	}
 
 	onunload() {
